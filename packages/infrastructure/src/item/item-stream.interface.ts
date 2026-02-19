bb7121e20e755de0fd1a6ddaad0e825e247c9951
masonJS <mwoodev526@gmail.com>
2026-02-19 18:16:18 +0900

    feat(infrastructure): port ItemStream interface

---
+++ packages/infrastructure/src/item/item-stream.interface.ts
@@ -0,0 +1,9 @@
+import type { ExecutionContext } from "./execution-context";
+
+export interface ItemStream {
+	open?(executionContext: ExecutionContext): void;
+
+	update?(executionContext: ExecutionContext): void;
+
+	close?(): void;
+}
