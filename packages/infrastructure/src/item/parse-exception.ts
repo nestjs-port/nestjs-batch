080759888601dd43795b358efc863a1ea067e62e
Jake Son <sonjeabin@gmail.com>
2026-02-26 12:41:57 +0900

    fix: remove redundant exception constructors
    - Remove pass-through constructors flagged by biome noUselessConstructor
    
    - Simplify exception subclasses in core and infrastructure packages
    
    - Keep exception inheritance behavior unchanged

--- packages/infrastructure/src/item/parse-exception.ts
+++ packages/infrastructure/src/item/parse-exception.ts
@@ -1,7 +1,3 @@
 import { ItemReaderException } from "./item-reader-exception";
 
-export class ParseException extends ItemReaderException {
-  constructor(message: string, cause?: unknown) {
-    super(message, cause);
-  }
-}
+export class ParseException extends ItemReaderException {}
--- packages/infrastructure/src/item/unexpected-input-exception.ts
+++ packages/infrastructure/src/item/unexpected-input-exception.ts
@@ -1,7 +1,3 @@
 import { ItemReaderException } from "./item-reader-exception";
 
-export class UnexpectedInputException extends ItemReaderException {
-  constructor(message: string, cause?: unknown) {
-    super(message, cause);
-  }
-}
+export class UnexpectedInputException extends ItemReaderException {}
