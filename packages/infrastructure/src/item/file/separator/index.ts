6e9f792d48baac445df85c964ffcb52ea139ee9a
masonJS <mwoodev526@gmail.com>
2026-03-06 11:36:37 +0900

    feat(infrastructure): add record separator policy implementations

--- packages/infrastructure/src/item/file/separator/index.ts
+++ packages/infrastructure/src/item/file/separator/index.ts
@@ -1,2 +1,4 @@
+export { DefaultRecordSeparatorPolicy } from "./default-record-separator-policy";
 export type { RecordSeparatorPolicy } from "./record-separator-policy.interface";
 export { SimpleRecordSeparatorPolicy } from "./simple-record-separator-policy";
+export { SuffixRecordSeparatorPolicy } from "./suffix-record-separator-policy";
