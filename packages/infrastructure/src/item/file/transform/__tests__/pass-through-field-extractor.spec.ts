f2af6e8b8480332067a4f0da24561da61232784a
masonJS <mwoodev526@gmail.com>
2026-03-11 15:43:54 +0900

    refactor(infrastructure): improve flat file transform and add tests

---
+++ packages/infrastructure/src/item/file/transform/__tests__/pass-through-field-extractor.spec.ts
@@ -0,0 +1,44 @@
+import { describe, expect, it } from "vitest";
+import type { FieldSet } from "../field-set.interface";
+import { PassThroughFieldExtractor } from "../pass-through-field-extractor";
+
+describe("PassThroughFieldExtractor", () => {
+  it("test extract string", () => {
+    const extractor = new PassThroughFieldExtractor<string>();
+    const result = extractor.extract("abc");
+    expect(result).toEqual(["abc"]);
+  });
+
+  it("test extract array", () => {
+    const extractor = new PassThroughFieldExtractor<(string | null)[]>();
+    const result = extractor.extract(["a", "b", null, "d"]);
+    expect(result).toEqual(["a", "b", null, "d"]);
+  });
+
+  it("test extract field set", () => {
+    const extractor = new PassThroughFieldExtractor<FieldSet>();
+    const fieldSet = { values: ["a", "b", "", "d"] } as FieldSet;
+    const result = extractor.extract(fieldSet);
+    expect(result).toEqual(["a", "b", "", "d"]);
+  });
+
+  it("test extract collection", () => {
+    const extractor = new PassThroughFieldExtractor<(string | null)[]>();
+    const result = extractor.extract(["a", "b", null, "d"]);
+    expect(result).toEqual(["a", "b", null, "d"]);
+  });
+
+  it("test extract map", () => {
+    const extractor = new PassThroughFieldExtractor<
+      Map<string, string | null>
+    >();
+    const map = new Map<string, string | null>([
+      ["A", "a"],
+      ["B", "b"],
+      ["C", null],
+      ["D", "d"],
+    ]);
+    const result = extractor.extract(map);
+    expect(result).toEqual(["a", "b", null, "d"]);
+  });
+});
